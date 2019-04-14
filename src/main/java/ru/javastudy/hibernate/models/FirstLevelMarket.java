3b286c47361d76f2c2da753d77924e52ba744872
Daniil <stoletov.dk@gmail.com>
2019-04-14 20:47:39 +0300

    Добавлены рынки первого уровня, а именно:модели, сервисы, дао.

--- src/main/java/ru/javastudy/hibernate/models/FirstLevelMarket.java
+++ src/main/java/ru/javastudy/hibernate/models/FirstLevelMarket.java
@@ -1,7 +1,7 @@
 package ru.javastudy.hibernate.models;
 
 import javax.persistence.*;
-//категории рынков новые
+//категории рынков новы
 @Entity
 @Table(name = "first_level_market")
 public class FirstLevelMarket {
