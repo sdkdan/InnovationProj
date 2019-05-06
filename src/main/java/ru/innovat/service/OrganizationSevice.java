e8f979e0da49417110851de242605a4c936c44d6
Sariman56 <er56s@yandex.ru>
2019-05-06 14:53:09 +0300

    1
    1

--- src/main/java/ru/innovat/service/OrganizationSevice.java
+++ src/main/java/ru/innovat/service/OrganizationSevice.java
@@ -1,7 +1,6 @@
 package ru.innovat.service;
 
 
-import org.graalvm.compiler.core.common.type.ArithmeticOpTable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
